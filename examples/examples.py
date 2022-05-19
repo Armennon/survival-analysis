89c08a14942070ddd68646e2287d2ff417b35aed
Armennon <57568491+Armennon@users.noreply.github.com>
2022-05-19 21:30:22 +0400

    Update and rename surv.py to examples.py

--- examples/examples.py
+++ examples/examples.py
@@ -1,12 +1,12 @@
-import surival_analysis.model.*
-import survival_analysis.visualizations.*
+from survival_analysis import *
 
 data = 'https://raw.githubusercontent.com/Armennon/survival-analysis/main/examples/telco.csv'
 
-survival = data_dummy('telco.csv')
-categorical = get_cat('telco.csv')
+survival = data_dummy(data)
+categorical = get_cat(data)
 models_list = model_builder(survival)
 optimal_model = choose_optimal_model(models_list)
+optimal_model_name = optimal_model_name(optimal_model)
 aft_optimal_model = find_aft_model(optimal_model_name, survival)
 plot_survival_function = plot_survival_function(optimal_model)
 plot_hazard_function = plot_hazard_function(optimal_model)
@@ -18,8 +18,9 @@ categ_plots = clv_categorical_plots(survival, categorical)
 
 
 
-print(f'{optimal_model_name} is the the most optimal model for the given dataset.')
-plot_survival_function
-plot_hazard_function
-clv_dist
-categ_plots
+# print(f'{optimal_model_name} is the the most optimal model for the given dataset.')
+# print(survival_with_clv)
+# print(plot_survival_function)
+# print(plot_hazard_function)
+# print(clv_dist)
+# print(categ_plots)
