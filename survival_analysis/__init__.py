94ed6e5a61378a7806aa49aa23f9a7f7c357d708
Armennon <57568491+Armennon@users.noreply.github.com>
2022-05-19 17:32:37 +0400

    Update __init__.py

--- survival_analysis/__init__.py
+++ survival_analysis/__init__.py
@@ -1,1 +1,12 @@
+__doc__ = """
+Survival Analysis
+========
+A package for implementing Survival analysis algorithm for telecom companies and not only.
+"""
 
+__author__ = """Armine Papikyan"""
+__version__ = "0.0.1"
+
+from . import model  # noqa : F401
+from . import visualisation  # noqa : F401
+from . import utils # noqa : F401
