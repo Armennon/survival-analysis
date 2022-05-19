8d7b93933f388d8ba63a941e59244463c9c9f2eb
Armennon <57568491+Armennon@users.noreply.github.com>
2022-05-19 21:33:01 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 # Adding install requirements
-PACKAGE_DATA = [, ]
-
+lif = {"lifelines": ["datasets/*"]}
 setup(
     author="Armine Papikyan",
     description="A package for implementing Survival Analysis.",
     name="survival-analysis",
     packages=find_packages(include=["survival_analysis", "survival_analysis.*"]),
     version="0.0.1",
-    install_requires=["lifelines>=0.27.0": ["datasets/*"], "numpy>=1.22.3", "pandas>=1.4.2", "seaborn>=0.11.2", "matplotlib>=3.2.2"],
+    install_requires=["numpy>=1.22.3", "pandas>=1.4.2", "seaborn>=0.11.2", "matplotlib>=3.2.2"],
+    package_data=lif,
     python_requires=">=3.8"
 )
